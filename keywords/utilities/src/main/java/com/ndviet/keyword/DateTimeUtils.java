f4cae8e76b2b762fd5ec4f8dcfe4cdf198e8afac
Nguyen Duc Viet <nguyenducviet4496@gmail.com>
2022-12-28 16:55:02 +0700

    Added DateTimeUtils

---
+++ keywords/utilities/src/main/java/com/ndviet/keyword/DateTimeUtils.java
@@ -0,0 +1,24 @@
+package com.ndviet.keyword;
+
+import com.ndviet.libary.DateTime.DateTimeHelpers;
+import org.robotframework.javalib.annotation.RobotKeyword;
+import org.robotframework.javalib.library.AnnotationLibrary;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class DateTimeUtils extends AnnotationLibrary {
+    public DateTimeUtils() {
+        super(Arrays.asList("com/ndviet/keyword/DateTimeUtils.class"));
+    }
+
+    @RobotKeyword
+    public static Map getCurrentQuarterYear() {
+        return DateTimeHelpers.getCurrentQuarterYear();
+    }
+
+    @RobotKeyword
+    public static String getCurrentDateInFormat(String format) {
+        return DateTimeHelpers.getCurrentDateInFormat(format, 0);
+    }
+}
